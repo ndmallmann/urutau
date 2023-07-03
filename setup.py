1ed54b1caf18610e7d8f545a32d3d56767d1e897
Nicolas Dullius Mallmann <ndmallmann@outlook.com>
2023-07-02 21:42:21 -0300

    changed folder name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name="urutau",
     version="0.51",
-    packages=find_packages(exclude=['main.py']),
+    packages=find_packages(),
     author="Nicolas Dullius Mallmann",
     python_requires=">=3.10",
 )
