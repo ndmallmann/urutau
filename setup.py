42e61a1bc7a5e32d09bfa0465f08836d1f654c94
Rogerio Riffel <riffel@ufrgs.br>
2024-03-06 13:55:45 -0300

    Version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="urutau",
-    version="1.001",
+    version="1.002",
     packages=find_packages(),
     author="Nicolas Dullius Mallmann",
     python_requires=">=3.10",
